2e9b3cc483b0717816ce9d5fbfaabeee07b83db6
exiao <huaxiao@umich.edu>
2020-01-27 16:57:15 -0800

    add html and bubble js

---
+++ bubble.js
@@ -0,0 +1,12 @@
+// import * as d3 from "d3";
+
+// Russia = combined counts for Soviet Union, Russian Empire, Russia, Unified Team
+// Germany = combined counts for West Germany, East Germany, United Team of Germany, Germany, Saar
+// Serbia = combined counts for Yugoslavia, Independent Olympic Participants, Serbia and Montenegro
+
+document.addEventListener("DOMContentLoaded", function(){
+    const root = document.getElementById('root');
+    root.textContent = 'This is new text';
+    d3.select("div").style("color", "red");
+    // console.log(d3);
+})
